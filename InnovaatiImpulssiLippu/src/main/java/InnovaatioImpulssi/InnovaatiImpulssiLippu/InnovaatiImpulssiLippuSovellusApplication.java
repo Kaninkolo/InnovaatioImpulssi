fb6e111c644aa5617a9b9a08a54d853604ff82b1
Emilia Friman <emilia.friman9795@gmail.com>
2024-02-03 22:56:30 +0200

    spring init lippusovellus

---
+++ InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/InnovaatiImpulssiLippuSovellusApplication.java
@@ -0,0 +1,13 @@
+package InnovaatioImpulssi.InnovaatiImpulssiLippu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaatiImpulssiLippuSovellusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaatiImpulssiLippuSovellusApplication.class, args);
+	}
+
+}
---
+++ InnovaatiImpulssiLippu/src/test/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/InnovaatiImpulssiLippuSovellusApplicationTests.java
@@ -0,0 +1,13 @@
+package InnovaatioImpulssi.InnovaatiImpulssiLippu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaatiImpulssiLippuSovellusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
