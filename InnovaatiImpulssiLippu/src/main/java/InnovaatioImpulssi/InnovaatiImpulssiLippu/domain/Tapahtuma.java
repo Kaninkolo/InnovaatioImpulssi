d5c2a2ffd3cc3456d2831e544b4bc87484d740f2
Niilo Virtamo <niilo.virtamo@saunalahti.fi>
2024-02-09 16:01:08 +0200

    lisätty .web ja .domain kansiot, tehty Tapahtuma-luokka sekä tyhjä Controller

---
+++ InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/domain/Tapahtuma.java
@@ -0,0 +1,56 @@
+package InnovaatioImpulssi.InnovaatiImpulssiLippu.domain;
+
+import java.util.Date;
+
+public class Tapahtuma {
+
+  private long tapahtuma_id;
+  private Date pvm;
+  private String sijainti;
+  private String kuvaus;
+
+  public Tapahtuma(long tapahtuma_id, Date pvm, String sijainti, String kuvaus) {
+    this.tapahtuma_id = tapahtuma_id;
+    this.pvm = pvm;
+    this.sijainti = sijainti;
+    this.kuvaus = kuvaus;
+  }
+
+  public long getTapahtuma_id() {
+    return tapahtuma_id;
+  }
+
+  public void setTapahtuma_id(long tapahtuma_id) {
+    this.tapahtuma_id = tapahtuma_id;
+  }
+
+  public Date getPvm() {
+    return pvm;
+  }
+
+  public void setPvm(Date pvm) {
+    this.pvm = pvm;
+  }
+
+  public String getSijainti() {
+    return sijainti;
+  }
+
+  public void setSijainti(String sijainti) {
+    this.sijainti = sijainti;
+  }
+  public String getKuvaus() {
+    return kuvaus;
+  }
+
+  public void setKuvaus(String kuvaus) {
+    this.kuvaus = kuvaus;
+  }
+
+  @Override
+  public String toString() {
+    return "Tapahtuma [tapahtuma_id=" + tapahtuma_id + ", pvm=" + pvm + ", sijainti=" + sijainti + ", kuvaus=" + kuvaus
+        + "]";
+  }
+  
+}
---
+++ InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/web/TicketGuruController.java
@@ -0,0 +1,8 @@
+package InnovaatioImpulssi.InnovaatiImpulssiLippu.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TicketGuruController {
+
+}
