a161c9de35597f51738a6c84a790ee82e59f2f79
Jasmin Huttunen <jasmin.huttunen@protonmail.com>
2024-02-11 15:19:39 +0200

    Lippu entity

---
+++ InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/domain/Lippu.java
@@ -0,0 +1,15 @@
+package InnovaatioImpulssi.InnovaatiImpulssiLippu.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Lippu {
+
+    @Id
+    private Long lippu_id;
+
+
+}
