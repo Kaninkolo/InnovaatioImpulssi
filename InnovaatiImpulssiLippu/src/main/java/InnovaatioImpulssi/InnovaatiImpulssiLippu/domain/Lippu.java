a197a3864fa8c47e7a01807b8289c19185089ccb
Jasmin Huttunen <jasmin.huttunen@protonmail.com>
2024-02-12 12:07:01 +0200

    lippu

--- InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/domain/Lippu.java
+++ InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/domain/Lippu.java
@@ -1,9 +1,6 @@
 package InnovaatioImpulssi.InnovaatiImpulssiLippu.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Lippu {
@@ -11,5 +8,36 @@ public class Lippu {
     @Id
     private Long lippu_id;
 
+    @ManyToMany
+    @JoinColumn(name = lippu_id)
+    private LippuTyyppi lippuTyyppi;
 
+    /// @ManyToOne
+    // @JoinColumn(name = tilausnumero)
+    // private Tilausnumero tilausnumero;
+
+
+    public Long getLippu_id() {
+        return lippu_id;
+    }
+
+    public void setLippu_id(Long lippu_id) {
+        this.lippu_id = lippu_id;
+    }
+
+    public LippuTyyppi getLippuTyyppi() {
+        return lippuTyyppi;
+    }
+
+    public void setLippuTyyppi(LippuTyyppi lippuTyyppi) {
+        this.lippuTyyppi = lippuTyyppi;
+    }
+
+    @Override
+    public String toString() {
+        return "Lippu{" +
+                "lippu_id=" + lippu_id +
+                ", lippuTyyppi=" + lippuTyyppi +
+                '}';
+    }
 }
