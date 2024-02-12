1acd03ab421de99295e72569c1ee666d04100ff7
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-02-12 17:47:20 +0200

    Myyjä repo lisätty
    Lisäsin Myyjän ja repon sille, toivottavasti meni edes jotenkin oikein

---
+++ InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/domain/Myyja.java
@@ -0,0 +1,39 @@
+package InnovaatioImpulssi.InnovaatiImpulssiLippu.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Myyja {
+    @Id
+    private long myyja_id;
+
+    private String nimi;
+
+    public Myyja(long myyja_id, String nimi) {
+        this.myyja_id = myyja_id;
+        this.nimi = nimi;
+    }
+
+    public long getMyyja_id() {
+        return myyja_id;
+    }
+
+    public void setMyyja_id(long myyja_id) {
+        this.myyja_id = myyja_id;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    @Override
+    public String toString() {
+        return "Myyja [myyja_id=" + myyja_id + ", nimi=" + nimi + "]";
+    }
+
+}
