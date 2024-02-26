2819866c5a25a32d4e3bb8f85d961475f39bd84d
Emilia Friman <emilia.friman9795@gmail.com>
2024-02-26 20:35:10 +0200

    deletemapping

--- InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/web/TapahtumaController.java
+++ InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/web/TapahtumaController.java
@@ -1,10 +1,10 @@
 package InnovaatioImpulssi.InnovaatiImpulssiLippu.web;
 
+import InnovaatioImpulssi.InnovaatiImpulssiLippu.domain.TapatumaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import InnovaatioImpulssi.InnovaatiImpulssiLippu.domain.Tapahtuma;
 
@@ -15,6 +15,8 @@ import java.util.concurrent.atomic.AtomicLong;
 @RequestMapping("/tapahtuma")
 public class TapahtumaController {
 
+    @Autowired
+    TapatumaRepository tapatumaRepository;
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping
@@ -24,5 +26,15 @@ public class TapahtumaController {
     ) {
         return new Tapahtuma(counter.incrementAndGet(), pvm, "sijainti", kuvaus);
     }
+    @DeleteMapping("/{tapahtuma_id}")
+    public ResponseEntity<Void> deleteTapahtuma(@PathVariable("tapahtuma_id") Long tapahtuma_id){
+        if (tapatumaRepository.existsById(tapahtuma_id)){
+            tapatumaRepository.deleteById(tapahtuma_id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
 }
 
