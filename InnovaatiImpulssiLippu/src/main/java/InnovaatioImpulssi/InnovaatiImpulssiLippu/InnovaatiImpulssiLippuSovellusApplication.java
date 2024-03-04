930e1a1d85ed3541a5f795b89e0c353955c8d932
Jasmin Huttunen <jasmin.huttunen@protonmail.com>
2024-03-04 13:30:29 +0200

    commandlinerunner tapahtumalle

--- InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/InnovaatiImpulssiLippuSovellusApplication.java
+++ InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/InnovaatiImpulssiLippuSovellusApplication.java
@@ -8,6 +8,11 @@ import org.springframework.context.annotation.Bean;
 import InnovaatioImpulssi.InnovaatiImpulssiLippu.domain.LippuRepository;
 import InnovaatioImpulssi.InnovaatiImpulssiLippu.domain.Myyja;
 import InnovaatioImpulssi.InnovaatiImpulssiLippu.domain.MyyjaRepository;
+import InnovaatioImpulssi.InnovaatiImpulssiLippu.domain.Tapahtuma;
+import InnovaatioImpulssi.InnovaatiImpulssiLippu.domain.TapatumaRepository;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import org.springframework.boot.CommandLineRunner;
 
@@ -23,6 +28,18 @@ public class InnovaatiImpulssiLippuSovellusApplication {
 		return (args) -> {
 
 			myyjarepository.save(new Myyja("Pekka Puupää"));
+		};
+	}
+
+	@Bean
+	public CommandLineRunner tapahtuma(TapatumaRepository tapahtumarepository) {
+		return (args) -> {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+			Tapahtuma tapahtuma = new Tapahtuma(null, sdf.parse("15-03-2022"), "Helsinki", "Kevätkonsertti",
+					new ArrayList<>());
+			tapahtumarepository.save(tapahtuma);
+
 		};
 	}
 
