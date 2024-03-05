686b780f693edbb970d946cca8a1d49964e682ce
Niilo Virtamo <niilo.virtamo@saunalahti.fi>
2024-03-05 11:09:10 +0200

    Lisätty tapahtumat.html

--- InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/web/TicketGuruController.java
+++ InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/web/TicketGuruController.java
@@ -1,8 +1,25 @@
 package InnovaatioImpulssi.InnovaatiImpulssiLippu.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import InnovaatioImpulssi.InnovaatiImpulssiLippu.domain.TapatumaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class TicketGuruController {
+
+  @Autowired
+  private TapatumaRepository trepository;
+
+  @GetMapping("/tapahtumat")
+  public String haeTapahtumat(Model model) {
+      model.addAttribute("tapahtumat", trepository.findAll());
+      return "tapahtumat";
+  }
+  
 
 }
