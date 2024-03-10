eced0303792d6df369968c395500b677e3e97bca
Niilo Virtamo <niilo.virtamo@saunalahti.fi>
2024-03-10 14:50:36 +0200

    LippuControlleriin CRUD

--- InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/web/LippuController.java
+++ InnovaatiImpulssiLippu/src/main/java/InnovaatioImpulssi/InnovaatiImpulssiLippu/web/LippuController.java
@@ -3,33 +3,57 @@ package InnovaatioImpulssi.InnovaatiImpulssiLippu.web;
 import org.springframework.web.bind.annotation.RestController;
 
 import InnovaatioImpulssi.InnovaatiImpulssiLippu.domain.Lippu;
-import InnovaatioImpulssi.InnovaatiImpulssiLippu.service.TicketService;
+import InnovaatioImpulssi.InnovaatiImpulssiLippu.domain.Tapahtuma;
+import InnovaatioImpulssi.InnovaatiImpulssiLippu.service.LippuService;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/api/tickets")
+@RequestMapping("/api/liput")
 public class LippuController {
-  
+
   @Autowired
-  TicketService service;
+  LippuService service;
 
   @GetMapping
   public List<Lippu> getAllLippu() {
-      return service.getAllLippu();
+    return service.getAllLippu();
   }
 
   @GetMapping("/{id}")
   public Optional<Lippu> getLippuById(Long id) {
     return service.getLippuById(id);
   }
-  
+
+  @PostMapping
+  public ResponseEntity<Lippu> saveLippu(@RequestBody Lippu lippu) {
+    Lippu uusiLippu = service.saveLippu(lippu);
+    return ResponseEntity.status(HttpStatus.CREATED).body(uusiLippu);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Lippu> updateLippu(@PathVariable Long id,
+      @RequestBody Lippu lippuBody) {
+    Lippu updatedLippu = service.updateLippu(id, lippuBody);
+    return ResponseEntity.ok(updatedLippu);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteLippu(@PathVariable("id") Long id) {
+    service.deleteLippu(id);
+    return ResponseEntity.ok().build();
+  }
 
 }
